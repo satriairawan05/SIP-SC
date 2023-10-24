eba0ee18082fe622cbc7acc9d76d9b17dea9994e
satriairawan05 <satriai418@gmail.com>
2023-10-24 14:47:15 +0800

    User & Role Permission

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -60,6 +60,6 @@ class LoginController extends Controller
         $this->guard()->logout();
         $request->session()->invalidate();
 
-        return $this->loggedOut($request) ?: redirect('home-page');
+        return $this->loggedOut($request) ?: redirect('login');
     }
 }
--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -1,23 +1,14 @@
 @extends('backend.layout.app')
 
 @section('bradcrumb')
-    <div class="row align-items-center">
-        <div class="col-md-8">
-            <div class="page-header-title">
-                <h5 class="m-b-10">Dashboard</h5>
-                <p class="m-b-0">Sistem Informasi Pengajuan Surat Cuti Berbasis
-                    Web</p>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <ul class="breadcrumb-title">
-                <li class="breadcrumb-item">
-                    <a href="#"> <i class="fa fa-home"></i> </a>
-                </li>
-                <li class="breadcrumb-item"><a href="#">{{ $name }}</a>
-                </li>
-            </ul>
-        </div>
+    <div class="col-md-4">
+        <ul class="breadcrumb-title">
+            <li class="breadcrumb-item">
+                <a href="{{ route('dashboard') }}"> <i class="fa fa-home"></i> </a>
+            </li>
+            <li class="breadcrumb-item"><a href="#">{{ $name }}</a>
+            </li>
+        </ul>
     </div>
 @endsection
 
@@ -26,7 +17,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h1 class="h4 text-bold text-center">Selamat Datang, Dina Minarti </h1>
+                    <h1 class="h4 text-bold text-center">Selamat Datang, {{ auth()->user()->name }}.</h1>
                 </div>
             </div>
         </div>
