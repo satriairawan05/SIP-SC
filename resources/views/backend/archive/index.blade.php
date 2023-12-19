5a2366a4056b52bdacbc182aa88af4055cce9551
satriairawan05 <satriai418@gmail.com>
2023-12-19 13:34:41 +0800

    Finishing #1.6

--- resources/views/backend/archive/index.blade.php
+++ resources/views/backend/archive/index.blade.php
@@ -18,17 +18,35 @@
             <div class="card">
                 <div class="card-body">
                     <div class="list-group">
-                        @foreach ($departemen as $d)
-                        @php
-                            $scCount = \App\Models\SuratCuti::where('departemen_id', $d->departemen_id)->whereNotNull('sc_no_surat')->count();
-                        @endphp
-                            <a href="?departemen_id={!! $d->departemen_id !!}"
-                                class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">{{ $d->departemen_name }} <span
-                                    class=" badge rounded-pill @if ($scCount == 0) bg-secondary @else bg-primary @endif">
+                        @if (auth()->user()->departemen_id != null)
+                            @php
+                                $scCount = \App\Models\SuratCuti::where('departemen_id', auth()->user()->departemen_id)
+                                    ->whereNotNull('sc_no_surat')
+                                    ->count();
+                            @endphp
+                            <a href="?departemen_id={!! auth()->user()->departemen_id !!}"
+                                class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">{{ $d->departemen_name }}
+                                <span
+                                    class="badge rounded-pill @if ($scCount == 0) bg-secondary @else bg-primary @endif">
                                     {{ $scCount == 0 ? 'Empty Data' : $scCount }}
                                 </span>
                             </a>
-                        @endforeach
+                        @else
+                            @foreach ($departemen as $d)
+                                @php
+                                    $scCount = \App\Models\SuratCuti::where('departemen_id', $d->departemen_id)
+                                        ->whereNotNull('sc_no_surat')
+                                        ->count();
+                                @endphp
+                                <a href="?departemen_id={!! $d->departemen_id !!}"
+                                    class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">{{ $d->departemen_name }}
+                                    <span
+                                        class="badge rounded-pill @if ($scCount == 0) bg-secondary @else bg-primary @endif">
+                                        {{ $scCount == 0 ? 'Empty Data' : $scCount }}
+                                    </span>
+                                </a>
+                            @endforeach
+                        @endif
                     </div>
                 </div>
             </div>
