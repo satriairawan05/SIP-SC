311b29e66b53ea78045778b34abb1046b6d9d68f
satriairawan05 <satriai418@gmail.com>
2024-01-10 20:08:53 +0800

    Fix Print Function

--- resources/views/backend/surat_cuti/index2.blade.php
+++ resources/views/backend/surat_cuti/index2.blade.php
@@ -215,47 +215,49 @@
         });
 
         const printDoc = (id) => {
-                var contents = "";
-                var url = "{{ route('surat_cuti.show', ':id') }}";
-                url = url.replace(':id', id);
-                $.get(url, function(data, status) {
-                            contents = data;
-                            var frame1 = $('<iframe />');
-                            frame1[0].name = "frame1";
-                            frame1.css({
-                                "position": "absolute",
-                                "top": "-1000000px"
-                            });
-                            $("body").append(frame1);
-                            var frameDoc = frame1[0].contentWindow ? frame1[0].contentWindow : frame1[0].contentDocument
-                                .document ?
-                                frame1[0].contentDocument.document : frame1[0].contentDocument;
-                            frameDoc.document.open();
-                            frameDoc.document.write(`
+            const url = "{{ route('surat_cuti.show', ':id') }}";
+
+            $.get(url, function(data, status) {
+                const contents = data;
+
+                const frame1 = $('<iframe />', {
+                    name: 'frame1',
+                    css: {
+                        position: 'absolute',
+                        top: '-1000000px'
+                    }
+                });
+
+                $('body').append(frame1);
+
+                const frameDoc = frame1[0].contentDocument || frame1[0].contentWindow.document;
+                frameDoc.open();
+                frameDoc.write(`
             <!DOCTYPE html>
             <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>${process.env.APP_NAME}</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="${asset('images/apple-touch-icon.png')}">
+                <link rel="icon" type="image/png" sizes="32x32" href="${asset('images/favicon-32x32.png')}">
+                <link rel="icon" type="image/png" sizes="16x16" href="${asset('images/favicon-16x16.png')}">
+                <link rel="icon" type="image/png" sizes="16x16" href="${asset('images/favicon.ico')}">
+            </head>
+            <body id='bodycontent'>
+                ${contents}
+            </body>
+            </html>
+        `);
+                frameDoc.close();
 
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                    <title>{{ env('APP_NAME') }}</title>
-                    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
-                    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
-                    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
-                    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.ico') }}">
-                </head>
-
-                <body id='bodycontent'>`);
-                            frameDoc.document.write(contents);
-                            frameDoc.document.write(`
-                </body>
-            </html>`);
-                            frameDoc.document.close();
-                            setTimeout(function() {
-                                window.frames["frame1"].focus();
-                                window.frames["frame1"].print();
-                                frame1.remove();
-                            }, 1000);
+                setTimeout(function() {
+                    window.frames['frame1'].focus();
+                    window.frames['frame1'].print();
+                    frame1.remove();
+                }, 1000);
+            });
+        };
     </script>
 @endsection
